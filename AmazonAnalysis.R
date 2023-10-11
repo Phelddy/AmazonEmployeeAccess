faf5031c72aa6eb435cc9908e63e0faa8af37b90
Jared Lane <jared@gimlisys.com>
2023-10-11 13:39:06 -0600

    10-11-2023 Penalized Logistic Regression

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -20,15 +20,17 @@ plot_missing(amazon_tr)
 amazon_EDA <- amazon_tr %>% mutate(RESOURCE = as.factor(RESOURCE), ACTION = as.factor(ACTION))
 ggplot(data = amazon_EDA) + geom_mosaic(aes(x = product(RESOURCE), fill = ACTION))
 
+amazon_tr <- amazon_tr %>% mutate(ACTION = as.factor(ACTION))
+
 my_recipe <- recipe(ACTION ~ ., data = amazon_tr) %>%
   step_mutate_at(all_numeric_predictors(), fn = factor) %>%
-  step_other(all_nominal_predictors(), threshold = .01) %>%
+  step_other(all_nominal_predictors(), threshold = .001) %>%
   step_dummy(all_nominal_predictors())
 prep <- prep(my_recipe)
 baked <- bake(prep, new_data = amazon_tr)
 baked <- bake(prep, new_data = amazon_te)
 
-logistic_mod <- logistic_red() %>%
+logistic_mod <- logistic_reg() %>%
   set_engine("glm")
 
 logistic_workflow <- workflow() %>%
@@ -39,3 +41,54 @@ logistic_workflow <- workflow() %>%
 amazon_predictions <- predict(logistic_workflow,
                               new_data = amazon_te,
                               type = "prob")
+
+#formats submissions properly
+submission <- bind_cols(amazon_te %>% select(id), amazon_predictions$.pred_1)
+submission <- submission %>% rename("Id" = "id", "Action" = "...2")
+#writes onto a csv
+vroom_write(submission, "./AmazonEmployeeAccess/AmazonEmployeeAccess/submission.csv", col_names = TRUE, delim = ", ")
+
+
+### 10-11-23 Penalized Logistic Regression
+my_recipe <- recipe(ACTION ~ ., data = amazon_tr) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = .001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
+prep <- prep(my_recipe)
+baked <- bake(prep, new_data = amazon_tr)
+baked <- bake(prep, new_data = amazon_te)
+
+plog_mod <- logistic_reg(mixture = tune(), penalty = tune()) %>%
+  set_engine("glmnet")
+
+amazon_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(plog_mod)
+
+##Tuning Grid
+
+tuning_grid <- grid_regular(penalty(),
+                            mixture(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_tr, v = 5, repeats = 1)
+
+CV_results <- amazon_workflow %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <- amazon_workflow %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = amazon_tr)
+
+amazon_predictions <- final_wf %>% predict(new_data = amazon_te, type = "prob")
+
+#formats submissions properly
+submission <- bind_cols(amazon_te %>% select(id), amazon_predictions$.pred_1)
+submission <- submission %>% rename("Id" = "id", "Action" = "...2")
+#writes onto a csv
+vroom_write(submission, "./AmazonEmployeeAccess/AmazonEmployeeAccess/submission.csv", col_names = TRUE, delim = ", ")
