f28894f7e7177de9b0e09db6114fb7700555e9be
Jared Lane <jared@gimlisys.com>
2023-10-23 13:46:02 -0600

    10-23-23 PCDR

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -134,5 +134,68 @@ submission <- submission %>% rename("Id" = "id", "Action" = "...2")
 #writes onto a csv
 vroom_write(submission, "./AmazonEmployeeAccess/AmazonEmployeeAccess/submission.csv", col_names = TRUE, delim = ", ")
 
+##Naive BAYES
+library(tidymodels)
 
+nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes")
+
+library(discrim)
+
+
+nb_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(nb_model)
+
+tuning_grid <- grid_regular(Laplace(),
+                            smoothness(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_tr, v = 5, repeats = 1)
+
+CV_results <- nb_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <- nb_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = amazon_tr)
+
+amazon_predictions <- final_wf %>% predict(new_data = amazon_te, type = "prob")
+
+#formats submissions properly
+submission <- bind_cols(amazon_te %>% select(id), amazon_predictions$.pred_1)
+submission <- submission %>% rename("Id" = "id", "Action" = "...2")
+#writes onto a csv
+vroom_write(submission, "./AmazonEmployeeAccess/AmazonEmployeeAccess/submission.csv", col_names = TRUE, delim = ", ")
+
+###PCA Dimension Reduction
+library(tidymodels)
+library(embed)
+library(vroom)
+library(ggmosaic)
+library(DataExplorer)
+library(GGally)
+library(patchwork)
+
+amazon_tr <- vroom("./AmazonEmployeeAccess/AmazonEmployeeAccess/train.csv")
+amazon_te <- vroom("./AmazonEmployeeAccess/AmazonEmployeeAccess/test.csv")
+
+
+amazon_tr <- amazon_tr %>% mutate(ACTION = as.factor(ACTION))
+
+my_recipe <- recipe(ACTION ~ ., data = amazon_tr) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = .001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
+  step_normalize(all_predictors()) %>%
+  step_pca(all_predictors(), threshold = .9)
+prep <- prep(my_recipe)
+baked <- bake(prep, new_data = amazon_tr)
+baked <- bake(prep, new_data = amazon_te)
 
