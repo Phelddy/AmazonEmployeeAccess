0774c463b3c66149b1b9b41565d8242830628e8d
Jared Lane <jared@gimlisys.com>
2023-10-25 13:45:10 -0600

    10/25/2023 SVM

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -199,3 +199,100 @@ prep <- prep(my_recipe)
 baked <- bake(prep, new_data = amazon_tr)
 baked <- bake(prep, new_data = amazon_te)
 
+#KNN
+knn <- nearest_neighbor(neighbors = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("kknn")
+
+
+
+knn_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(knn)
+
+tuning_grid <- grid_regular(neighbors(),
+                            levels = 5)
+
+folds <- vfold_cv(amazon_tr, v = 5, repeats = 1)
+
+CV_results <- knn_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <- knn_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = amazon_tr)
+
+amazon_predictions <- final_wf %>% predict(new_data = amazon_te, type = "prob")
+
+#formats submissions properly
+submission <- bind_cols(amazon_te %>% select(id), amazon_predictions$.pred_1)
+submission <- submission %>% rename("Id" = "id", "Action" = "...2")
+#writes onto a csv
+vroom_write(submission, "./AmazonEmployeeAccess/AmazonEmployeeAccess/submission.csv", col_names = TRUE, delim = ", ")
+
+
+#### SVM 
+library(tidymodels)
+library(embed)
+library(vroom)
+library(ggmosaic)
+library(DataExplorer)
+library(GGally)
+library(patchwork)
+
+amazon_tr <- vroom("./AmazonEmployeeAccess/AmazonEmployeeAccess/train.csv")
+amazon_te <- vroom("./AmazonEmployeeAccess/AmazonEmployeeAccess/test.csv")
+
+amazon_tr <- amazon_tr %>% mutate(ACTION = as.factor(ACTION))
+
+my_recipe <- recipe(ACTION ~ ., data = amazon_tr) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = .001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION)) %>%
+  step_normalize(all_predictors()) %>%
+  step_pca(all_predictors(), threshold = .8)
+prep <- prep(my_recipe)
+baked <- bake(prep, new_data = amazon_tr)
+baked <- bake(prep, new_data = amazon_te)
+
+svmRadial <- svm_rbf(rbf_sigma = tune(), cost = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("kernlab")
+
+svm_wf <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(svmRadial)
+
+tuning_grid <- grid_regular(rbf_sigma(),
+                            cost(),
+                            levels = 2)
+
+folds <- vfold_cv(amazon_tr, v = 4, repeats = 1)
+
+CV_results <- svm_wf %>%
+  tune_grid(resamples = folds,
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+bestTune <- CV_results %>%
+  select_best("roc_auc")
+
+final_wf <- svm_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = amazon_tr)
+
+amazon_predictions <- final_wf %>% predict(new_data = amazon_te, type = "prob")
+
+#formats submissions properly
+submission <- bind_cols(amazon_te %>% select(id), amazon_predictions$.pred_1)
+submission <- submission %>% rename("Id" = "id", "Action" = "...2")
+#writes onto a csv
+vroom_write(submission, "./AmazonEmployeeAccess/AmazonEmployeeAccess/submission.csv", col_names = TRUE, delim = ", ")
+
+
+
